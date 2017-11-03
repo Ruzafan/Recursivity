e208c8ad0c0f3a08f0bb5297334e1ee806d331e4
Ruzafan <marcramiro@gmail.com>
2017-11-03 08:22:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
     $(".circle").each(function(q,a){
         setTimeout(function(){ Move($(a))},1);
-        
     });
     
     function Move(obj){
         var position = obj.position();
-        var xMove = parseInt(obj.data("xmove"));
-        var yMove = parseInt(obj.data("ymove"));
+        var xMove = +obj.data("xmove");
+        var yMove = +obj.data("ymove");
+        
         if(position.top > 580 || position.top < 9)
             xMove = xMove * -1;
+        
         if(position.left > 780 || position.left < 9)
             yMove = yMove * -1;
         
@@ -20,7 +21,12 @@ $(document).ready(function(){
     }
 
     $(".addBall").on("click",function(){
-        $("#main-div").append('<div class="circle" data-xMove="1" data-yMove="1" style="top:'+Math.floor((Math.random() * 580) + 9)+'px;left:'+Math.floor((Math.random() * 760) + 9)+'px"></div>');
+        var pos = {
+            top: Math.floor((Math.random() * 580) + 9),
+            left: Math.floor((Math.random() * 760) + 9)
+        };
+        
+        $("#main-div").append('<div class="circle" data-xMove="1" data-yMove="1" style="top:'+pos.top+'px;left:'+pos.left+'px"></div>');
         setTimeout(function(){ Move($("#main-div .circle:last-child"))},1);
     });
     
